b3c6578ee2b46d61e927016771ab7b42eff9ab75
knightsy <peter.knight41@gmail.com>
2017-02-22 14:22:41 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name="microservice_functions",
+    version="0.0.1",
+    author="Peter Knight",
+    author_email="peter.knight41@gmail.com",
+    py_modules=['microservice_functions'],
+    license="MIT",
+
+    description="""
+        A collection of handy functions for making python microservices work in the real world
+    """
+
+)
